56c1ec3f46c50e3220f2db0efab07bf5dea771e4
eugene03850 <eugene385@ewhain.net>
2026-02-23 20:46:18 +0900

    comment: 9665, 23971번 주석 추가

--- yujin/week02/0223_23971.cpp
+++ yujin/week02/0223_23971.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 
 int main() {
-    double h,w,n,m;
-    cin>>h>>w>>n>>m;
+    // double h,w,n,m;
+    // cin>>h>>w>>n>>m;
 
-    int col = ceil(h/(n+1));
-    int row = ceil(w/(m+1));
+    // int col = ceil(h/(n+1));
+    // int row = ceil(w/(m+1));
 
+    // hwnm을 정수로 두고 올림공식 사용
+
+    int h,w,n,m;
+    cin>>h>>w>>n>>m; 
+
+    int col = (h+n)/(n+1);
+    int row = (w+m)/(m+1);
     cout<<col*row;
     
 
